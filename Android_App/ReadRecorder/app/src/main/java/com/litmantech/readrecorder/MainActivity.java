0e8160f9ed13526aac0d98666b4382ef1f6ca5d7
Jeff Jones <jeffthejava@gmail.com>
2016-05-19 03:23:54 -0400

    audio capturer and playback working!! when you boot the app it will play your mic audio in the speakers of the phone. and when you close the app it stops and open back up it starts

--- Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
+++ Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
@@ -1,13 +1,62 @@
 package com.litmantech.readrecorder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.litmantech.readrecorder.audio.Playback;
+import com.litmantech.readrecorder.audio.Recorder;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Recorder recorder;
+    private Playback playback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    55);
+
+        }
+
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        RunTest();
+    }
+
+    private void RunTest() {
+        if(playback == null)
+            playback = new Playback();
+        if(recorder == null)
+            recorder = new Recorder();
+
+        LinkedBlockingQueue audioListener  = playback.Play();
+        recorder.setAudioListener(audioListener);
+
+        recorder.Start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        recorder.Stop();
+        playback.Stop();
+
     }
 }
