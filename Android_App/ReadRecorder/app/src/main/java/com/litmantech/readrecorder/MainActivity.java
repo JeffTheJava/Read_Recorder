bb0a68b8131b9b895437e0b9a9223a0bf0baf40a
jeff@eventespresso.com <myevent@you1>
2016-05-20 03:16:34 -0400

    work on creating the core data structure. and tested some ui stuff with the next button. its working. keep in my this is nothing like what the real ui will look like. this is just a test

--- Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
+++ Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
@@ -6,18 +6,27 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.litmantech.readrecorder.audio.Playback;
 import com.litmantech.readrecorder.audio.Recorder;
+import com.litmantech.readrecorder.read.Session;
+import com.litmantech.readrecorder.read.line.Entry;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
     private Recorder recorder;
     private Playback playback;
     private boolean testAudioStuffON = false;
+    private Button prevBTN;
+    private Button nextBTN;
+    private Session session;
+    private TextView currentSentenceTXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,16 @@ public class MainActivity extends AppCompatActivity {
                     55);
         }
 
+        currentSentenceTXT = (TextView) findViewById(R.id.current_sentence);
+        prevBTN = (Button) findViewById(R.id.prev_btn);
+        nextBTN = (Button) findViewById(R.id.next_btn);
 
+        prevBTN.setOnClickListener(this);
+        nextBTN.setOnClickListener(this);
 
-
+        String[] mTestArray = getResources().getStringArray(R.array.testArray);
+        session = new Session(mTestArray);
+        UpdateUI();
     }
 
 
@@ -69,4 +85,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if(v == prevBTN){
+            session.PreviousEntry();
+        }else if(v == nextBTN){
+            session.NextEntry();
+        }
+
+        UpdateUI();
+    }
+
+    private void UpdateUI() {
+        Entry entry = session.getCurrentEntry();
+        currentSentenceTXT.setText(entry.getSentence());
+    }
 }
