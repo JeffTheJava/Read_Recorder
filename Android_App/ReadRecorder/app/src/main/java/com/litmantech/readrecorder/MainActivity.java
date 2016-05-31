f69fe8d0da54a726af33723967061637c0a893c5
JeffTheJava <JeffTheJava@gmail.com>
2016-05-30 23:32:37 -0400

    added an launcher icon. its a png. will make a mipmap out of it soon. for now it works.. updated the version number

--- Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
+++ Android_App/ReadRecorder/app/src/main/java/com/litmantech/readrecorder/MainActivity.java
@@ -7,11 +7,13 @@ import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//dont let phone sleep
 
         if (ContextCompat.checkSelfPermission(this,  Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
 
